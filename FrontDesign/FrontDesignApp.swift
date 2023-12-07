b1adf6fa60d447e613aac3ab9480ed880569009d
emilioberber <emilioberber@hotmail.com>
2023-12-07 11:49:50 -0600

    Initial Commit

---
+++ FrontDesign/FrontDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontDesignApp.swift
+//  FrontDesign
+//
+//  Created by Gdaalumno on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FrontDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
